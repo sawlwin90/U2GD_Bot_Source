6426c9549cd5f71b436b1fd8050e7a6619982840
sawlwin90 <74045088+sawlwin90@users.noreply.github.com>
2021-06-17 16:58:05 +0630

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,3 +1,21 @@
 class Creds():
     # ENTER Your bot Token Here
     TG_TOKEN = "1869229201:AAGHZMomOTaDqViewaJjlphBAuM6stwQmzg"
+    
+    
+    
+    #  Make Sure You Are Providing both value if you need Teamdrive upload
+    # Because of pydrive And pydrive v2 Api
+    
+    #Folder Id Of Teamdrive
+    TEAMDRIVE_FOLDER_ID = "1AFuFPZ9LnwfPHyg8mP4j6V7hUS3Su_jZ"
+    
+    # Id of Team drive 
+    TEAMDRIVE_ID = "0ACGfiHRwkZY0Uk9PVA"
+    
+    
+    
+    #Example 
+    #TG_TOKEN = "dkjfksdkffdkfdkfdj"
+    #TEAMDRIVE_FOLDER_ID = "13v4MaZnBz-iEHlZ0gFXk7rh"
+    #TEAMDRIVE_ID = "0APh6R4WVvguEUk9PV"
